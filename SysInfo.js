8a9770bbcb4cd66459335c004346683fa63759be
tayfunakgc <tayfun.akgc@gmail.com>
2020-07-21 23:03:47 +0300

    SysInfo@isRaspi

---
+++ SysInfo.js
@@ -0,0 +1,18 @@
+const si = require('systeminformation');
+
+const isRaspi = async () => {
+    try {
+        const osInfo = await si.osInfo();
+        if(osInfo.distro == 'Raspbian GNU/Linux') {
+            //console.log('it s raspi');
+            return true;
+        } else {
+            //console.log('it s not raspi');
+            return false;
+        }
+    } catch (err) {
+        console.log('SystemInfo@isRaspi Err: ', err);
+    }
+}
+
+module.exports.isRaspi = isRaspi;
